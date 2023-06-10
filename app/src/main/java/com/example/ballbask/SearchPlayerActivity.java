a56678573cd79117d655541d416ca2af228ef64c
Luís Davi Gomes de Santana <101807467+daviStudentDS@users.noreply.github.com>
2023-06-10 17:36:30 -0300

    Times

--- app/src/main/java/com/example/ballbask/SearchPlayerActivity.java
+++ app/src/main/java/com/example/ballbask/SearchPlayerActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PlayerActivity extends AppCompatActivity {
+public class SearchPlayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
+        setContentView(R.layout.activity_search_player);
     }
 }
